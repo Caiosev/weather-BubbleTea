7337a40e1855a6b45d86306df0944ef24a587e94
caiosev <kaiosev9@gmail.com>
2022-10-03 17:42:55 -0300

    implementando loading

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/Caiosev/weather-BubbleTea/metaweather"
+	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
@@ -12,9 +13,15 @@ import (
 func main() {
 	t := textinput.NewModel()
 	t.Focus()
+
+	s := spinner.NewModel()
+
+	s.Spinner = spinner.Globe
+
 	initialModel := model{
 		textInput: t,
 		typing:    true,
+		spinner:   s,
 	}
 	err := tea.NewProgram(initialModel, tea.WithAltScreen()).Start()
 	if err != nil {
@@ -29,6 +36,7 @@ func initialModel() model {
 
 type model struct {
 	textInput textinput.Model
+	spinner   spinner.Model
 
 	typing   bool
 	loading  bool
@@ -46,6 +54,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c":
 			return m, tea.Quit
+		case "enter":
+			m.typing = false
+			m.loading = true
+			return m, spinner.Tick
 		}
 	}
 
@@ -55,12 +67,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, cmd
 	}
 
+	if m.loading {
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+
 	return m, nil
 }
 
 func (m model) View() string {
 	if m.typing {
 		return fmt.Sprintf("Enter location:\n%s", m.textInput.View())
+	}
+
+	if m.loading {
+		return fmt.Sprintf("%s fetching weather ...", m.spinner.View())
 	}
 	return "Press Ctrl+C to exit"
 }
