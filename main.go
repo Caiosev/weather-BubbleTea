d2f1e9747e619bd0cc000e4959c608fa1ea9f694
caiosev <kaiosev9@gmail.com>
2022-10-03 17:32:17 -0300

    input no terminal

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/Caiosev/weather-BubbleTea/metaweather"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	err := tea.NewProgram(initialModel(), tea.WithAltScreen()).Start()
+	t := textinput.NewModel()
+	t.Focus()
+	initialModel := model{
+		textInput: t,
+		typing:    true,
+	}
+	err := tea.NewProgram(initialModel, tea.WithAltScreen()).Start()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -19,10 +27,17 @@ func initialModel() model {
 	return model{}
 }
 
-type model struct{}
+type model struct {
+	textInput textinput.Model
+
+	typing   bool
+	loading  bool
+	err      error
+	location metaweather.Location
+}
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -33,9 +48,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		}
 	}
+
+	if m.typing {
+		var cmd tea.Cmd
+		m.textInput, cmd = m.textInput.Update(msg)
+		return m, cmd
+	}
+
 	return m, nil
 }
 
 func (m model) View() string {
+	if m.typing {
+		return fmt.Sprintf("Enter location:\n%s", m.textInput.View())
+	}
 	return "Press Ctrl+C to exit"
 }
